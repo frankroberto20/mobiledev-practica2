4323d4ba38e90a66d992af2cc32fcf1ddc1e03a3
FRANK ROBERTO CHAVEZ SOSA <1085268@est.intec.edu.do>
2021-08-25 23:04:05 -0400

    First commit

---
+++ Login/Login/ViewModels/LoginViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Login.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Login.ViewModels
+{
+    class LoginViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public ICommand LoginCommand { private set; get; }
+        public ICommand SignUpNavigateCommand {private set; get; }
+
+        public async void OnLogin()
+        {
+            if (string.IsNullOrEmpty(Username) || string.IsNullOrEmpty(Password))
+            {
+                await App.Current.MainPage.DisplayAlert("Error", "User and Password fields can't be empty", "OK");
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("Welcome!", $"Hello {Username}", "OK");
+                await App.Current.MainPage.Navigation.PushAsync(new MainPage());
+            }
+        }
+
+        public async void SignUpNavigate()
+        {
+            await App.Current.MainPage.Navigation.PushAsync(new SignUpPage());
+        }
+        
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLogin);
+            SignUpNavigateCommand = new Command(SignUpNavigate);
+        }
+    }
+}
---
+++ Login/Login/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Login.ViewModels
+{
+    class SignUpViewModel
+    {
+
+    }
+}
