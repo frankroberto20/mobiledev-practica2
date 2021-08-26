d1e49f759346abd2f22fb413633f04bffc9ec722
frankchavez <robertofrank2000@hotmail.com>
2021-08-26 14:23:06 -0400

    Removed clutter from MenuView

--- Login/Login/Controls/MenuView.xaml.cs
+++ Login/Login/Controls/MenuView.xaml.cs
@@ -23,46 +23,10 @@ namespace Login.Controls
             typeof(MenuView),
             new MenuOption());
 
-        public static readonly BindableProperty MenuTitleProperty = BindableProperty.Create(
-            "MenuTitle",        // the name of the bindable property
-            typeof(string),     // the bindable property type
-            typeof(MenuView),   // the parent object type
-            string.Empty);      // the default value for the property
-
-        public static readonly BindableProperty MenuIconSourceProperty = BindableProperty.Create(
-            "MenuIconSource",        // the name of the bindable property
-            typeof(string),     // the bindable property type
-            typeof(MenuView),   // the parent object type
-            string.Empty);      // the default value for the property
-
-        public static readonly BindableProperty MenuDescriptionProperty = BindableProperty.Create(
-            "MenuDescription",        // the name of the bindable property
-            typeof(string),     // the bindable property type
-            typeof(MenuView),   // the parent object type
-            string.Empty);      // the default value for the property
-
         public MenuOption MenuOption
         {
             get => (MenuOption)GetValue(MenuOptionProperty);
             set => SetValue(MenuOptionProperty, value);
-        }
-
-        public string MenuTitle
-        {
-            get => (string)GetValue(MenuTitleProperty);
-            set => SetValue(MenuTitleProperty, value);
-        }
-
-        public string MenuIconSource
-        {
-            get => (string)GetValue(MenuIconSourceProperty);
-            set => SetValue(MenuIconSourceProperty, value);
-        }
-
-        public string MenuDescription
-        {
-            get => (string)GetValue(MenuDescriptionProperty);
-            set => SetValue(MenuDescriptionProperty, value);
         }
     }
 }
